ff446187ed5246165b4df6d84ba4716d2457f26b
Hai Dang <haidangltv@gmail.com>
2024-05-08 13:39:27 +0700

    fix/ fix api create workspace

--- app/Http/Controllers/Workspace/CreateWorkspace.php
+++ app/Http/Controllers/Workspace/CreateWorkspace.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Workspace;
 
 use App\Http\Controllers\Controller;
+use App\Models\UserInWorkspace;
 use App\Models\Workspace;
+use App\Models\WorkspaceRole;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
@@ -18,11 +20,15 @@ class CreateWorkspace extends Controller
             'cover_img' => ['nullable', 'image', 'max:5120'],
         ]);
 
-
         $workspace['cover_img'] = $this->uploadImage($request, 'cover_img');
         $workspace['owner_id'] = Auth::id();
 
         $workspace = Workspace::create($workspace);
+
+        $owner_role = $this->createDefaultRole($workspace);
+
+        $this->addRoleToOwner($workspace, $owner_role);
+
         return response([
             'message' => 'Workspace created successfully',
             'workspace' => $workspace,
@@ -35,5 +41,29 @@ class CreateWorkspace extends Controller
             return Storage::disk('minio')->put('Workspace_cover_img', $image);
         }
         return null;
+    }
+
+    private function createDefaultRole($workspace)
+    {
+        $owner_role = WorkspaceRole::create([
+            'name' => 'owner',
+            'workspace_id' => $workspace->id,
+            'create_board' => true,
+        ]);
+        WorkspaceRole::create([
+            'name' => 'everyone',
+            'workspace_id' => $workspace->id,
+            'create_board' => false,
+        ]);
+        return $owner_role;
+    }
+
+    private function addRoleToOwner($workspace, $owner_role)
+    {
+        UserInWorkspace::create([
+            'user_id' => Auth::id(),
+            'workspace_id' => $workspace->id,
+            'workspace_role_id' => $owner_role->id,
+        ]);
     }
 }
---
+++ database/factories/WorkspaceRoleFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\WorkspaceRole;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\WorkspaceRole>
+ */
+class WorkspaceRoleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = WorkspaceRole::class;
+
+    public function definition(): array
+    {
+        return [
+                // 
+            ];
+    }
+}
