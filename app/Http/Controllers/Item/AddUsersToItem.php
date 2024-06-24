a5c37ba661d193baafd8b013d486170e0b9debaf
Nguyễn  Kim. Hải Đăng <dangnkh@d-soft.com.vn>
2024-06-24 17:58:40 +0700

    feat: authorize user in item management

--- app/Http/Controllers/Item/AddUsersToItem.php
+++ app/Http/Controllers/Item/AddUsersToItem.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Item;
 
 use App\Http\Controllers\Controller;
+use App\Models\Item;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class AddUsersToItem extends Controller
@@ -13,6 +15,18 @@ class AddUsersToItem extends Controller
         $request->validate([
             'user_ids' => 'array',
         ]);
+
+        $item = Item::find($item_id);
+        if (!$item || !$this->checkPermission($item->container->board_id)) {
+            return response(
+                [
+                    'message' =>
+                        'You do not have permission to manage users in item',
+                ],
+                403
+            );
+        }
+
         $user_ids = $request->input('user_ids');
 
         DB::table('user_in_item')->insertOrIgnore(
@@ -30,5 +44,23 @@ class AddUsersToItem extends Controller
             ['message' => 'Users Added to Item successfully'],
             200
         );
+    }
+    private function checkPermission($board_id)
+    {
+        $user_id = Auth::id();
+
+        $item_member_management = DB::table('user_in_board')
+            ->join(
+                'board_roles',
+                'user_in_board.board_role_id',
+                '=',
+                'board_roles.id'
+            )
+            ->where('user_in_board.user_id', $user_id)
+            ->where('user_in_board.board_id', $board_id)
+            ->select('board_roles.item_member_management')
+            ->first()->item_member_management;
+
+        return $item_member_management;
     }
 }
--- app/Http/Controllers/Item/DeleteItemMember.php
+++ app/Http/Controllers/Item/DeleteItemMember.php
@@ -3,13 +3,26 @@
 namespace App\Http\Controllers\Item;
 
 use App\Http\Controllers\Controller;
+use App\Models\Item;
 use App\Models\UserInItem;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class DeleteItemMember extends Controller
 {
     public function __invoke(Request $request, string $item_id, string $user_id)
     {
+        $item = Item::find($item_id);
+        if (!$item || !$this->checkPermission($item->container->board_id)) {
+            return response(
+                [
+                    'message' =>
+                        'You do not have permission to manage users in item',
+                ],
+                403
+            );
+        }
         $user_in_item = UserInItem::where('item_id', $item_id)
             ->where('user_id', $user_id)
             ->first();
@@ -30,5 +43,23 @@ class DeleteItemMember extends Controller
             ],
             200
         );
+    }
+    private function checkPermission($board_id)
+    {
+        $user_id = Auth::id();
+
+        $item_member_management = DB::table('user_in_board')
+            ->join(
+                'board_roles',
+                'user_in_board.board_role_id',
+                '=',
+                'board_roles.id'
+            )
+            ->where('user_in_board.user_id', $user_id)
+            ->where('user_in_board.board_id', $board_id)
+            ->select('board_roles.item_member_management')
+            ->first()->item_member_management;
+
+        return $item_member_management;
     }
 }
